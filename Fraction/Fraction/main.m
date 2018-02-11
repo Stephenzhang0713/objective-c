f9ca9bb6e37e16d22df266d1b452cf58eb3b1c6a
张瀚 <stephenzhang0713@outlook.com>
2018-02-11 20:17:09 +0800

    1.0 release

---
+++ Fraction/Fraction/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Fraction
+//
+//  Created by 张瀚 on 2018/2/11.
+//  Copyright © 2018年 张瀚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
